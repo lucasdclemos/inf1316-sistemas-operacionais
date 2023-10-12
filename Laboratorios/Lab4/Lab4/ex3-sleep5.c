beca2f8e7e9ccf1b73cf85f8f52ca8676fda1da8
Lucas Lemos <62488737+lucasdclemos@users.noreply.github.com>
2023-10-12 16:54:26 -0300

    Add files via upload

---
+++ Laboratorios/Lab4/Lab4/ex3-sleep5.c
@@ -0,0 +1,42 @@
+// Lucas Demarco Cambraia Lemos - 2110013
+// Natalia Passini Grossmann - 2110456
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define EVER ;;
+
+void childhandler(int signo);
+int delay;
+
+int main (int argc, char *argv[]) {
+    pid_t pid;
+    signal(SIGCHLD, childhandler);
+    char* parametros[] = {"sleep5", NULL};
+    if ((pid = fork()) < 0) {
+        fprintf(stderr, "Erro ao criar filho\n");
+        exit(-1);
+    }
+    if (pid == 0)        
+        execve("sleep5", parametros, 0);
+    else{
+        sscanf(argv[1], "%d", &delay); 
+        sleep(delay);
+        printf("Program exceeded limit of %d seconds!\n", delay);
+        kill(pid, SIGKILL);
+    }
+    return 0;
+}
+
+void childhandler(int signo) {
+    int status;
+    pid_t pid = wait(&status);
+    printf("Child %d terminated within %d seconds com estado %d.\n", pid, delay, status);
+    exit(0);
+}
